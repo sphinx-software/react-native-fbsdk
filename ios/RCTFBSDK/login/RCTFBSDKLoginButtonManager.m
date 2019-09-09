00bd80281cd1293d8cc254add018224b885f128d
Rikky <son.levuthai@gmail.com>
2019-09-09 18:47:12 +0700

    Fixed error when login with Facebook on iOS

--- ios/RCTFBSDK/login/RCTFBSDKLoginButtonManager.m
+++ ios/RCTFBSDK/login/RCTFBSDKLoginButtonManager.m
@@ -19,7 +19,6 @@
 #import "RCTFBSDKLoginButtonManager.h"
 
 #import <React/RCTBridge.h>
-#import <React/RCTComponentEvent.h>
 #import <React/RCTEventDispatcher.h>
 #import <React/RCTUtils.h>
 #import <React/UIView+React.h>
@@ -34,9 +33,9 @@ RCT_EXPORT_MODULE(RCTFBLoginButton)
 
 - (UIView *)view
 {
-  FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
-  loginButton.delegate = self;
-  return loginButton;
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    loginButton.delegate = self;
+    return loginButton;
 }
 
 #pragma mark - Properties
@@ -45,46 +44,43 @@ RCT_EXPORT_VIEW_PROPERTY(permissions, NSStringArray)
 
 RCT_CUSTOM_VIEW_PROPERTY(loginBehaviorIOS, FBSDKLoginBehavior, FBSDKLoginButton)
 {
-  [view setLoginBehavior:json ? [RCTConvert FBSDKLoginBehavior:json] : FBSDKLoginBehaviorBrowser];
+    [view setLoginBehavior:json ? [RCTConvert FBSDKLoginBehavior:json] : FBSDKLoginBehaviorBrowser];
 }
 
 RCT_EXPORT_VIEW_PROPERTY(defaultAudience, FBSDKDefaultAudience)
 
 RCT_CUSTOM_VIEW_PROPERTY(tooltipBehaviorIOS, FBSDKLoginButtonTooltipBehavior, FBSDKLoginButton)
 {
-  [view setTooltipBehavior:json ? [RCTConvert FBSDKLoginButtonTooltipBehavior:json] : FBSDKLoginButtonTooltipBehaviorAutomatic];
+    [view setTooltipBehavior:json ? [RCTConvert FBSDKLoginButtonTooltipBehavior:json] : FBSDKLoginButtonTooltipBehaviorAutomatic];
 }
 
 #pragma mark - FBSDKLoginButtonDelegate
 
 - (void)loginButton:(FBSDKLoginButton *)loginButton didCompleteWithResult:(FBSDKLoginManagerLoginResult *)result error:(NSError *)error
 {
-  NSDictionary *body = @{
-    @"type": @"loginFinished",
-    @"error": error ? RCTJSErrorFromNSError(error) : [NSNull null],
-    @"result": error ? [NSNull null] : @{
-      @"isCancelled": @(result.isCancelled),
-      @"grantedPermissions": result.isCancelled ? [NSNull null] : result.grantedPermissions.allObjects,
-      @"declinedPermissions": result.isCancelled ? [NSNull null] : result.declinedPermissions.allObjects,
-    },
-  };
-
-  RCTComponentEvent *event = [[RCTComponentEvent alloc] initWithName:@"topChange"
-                                                             viewTag:loginButton.reactTag
-                                                                body:body];
-  [self.bridge.eventDispatcher sendEvent:event];
+    NSDictionary *event = @{
+                            @"type": @"loginFinished",
+                            @"target": loginButton.reactTag,
+                            @"error": error ? RCTJSErrorFromNSError(error) : [NSNull null],
+                            @"result": error ? [NSNull null] : @{
+                                    @"isCancelled": @(result.isCancelled),
+                                    @"grantedPermissions": result.isCancelled ? [NSNull null] : result.grantedPermissions.allObjects,
+                                    @"declinedPermissions": result.isCancelled ? [NSNull null] : result.declinedPermissions.allObjects,
+                                    },
+                            };
+    
+    [self.bridge.eventDispatcher sendInputEventWithName:@"topChange" body:event];
+    [self.bridge.eventDispatcher sendEvent:event];
 }
 
 - (void)loginButtonDidLogOut:(FBSDKLoginButton *)loginButton
 {
-  NSDictionary *body = @{
-    @"type": @"logoutFinished",
-  };
-
-  RCTComponentEvent *event = [[RCTComponentEvent alloc] initWithName:@"topChange"
-                                                             viewTag:loginButton.reactTag
-                                                                body:body];
-  [self.bridge.eventDispatcher sendEvent:event];
+    NSDictionary *event = @{
+                            @"target": loginButton.reactTag,
+                            };
+    
+    [self.bridge.eventDispatcher sendInputEventWithName:@"topChange" body:event];
+    [self.bridge.eventDispatcher sendEvent:event];
 }
 
 @end
